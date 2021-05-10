29cb551557d731f0883695cdc25dfa2c29a92242
Tiago Leal <tibeal@gmail.com>
2021-05-10 13:46:56 -0300

    fixed deployment

--- scripts/shell/deployProjectToOrg.sh
+++ scripts/shell/deployProjectToOrg.sh
@@ -4,8 +4,8 @@ if [ $# -eq 0 ]
 then
     echo 'ERROR: MISSION USERNAME'
 else
-    sfdx force:source:deploy -p force-app/ -u $1 -l RunSpecifiedTests -r TestF_PointsService -c
+    sfdx force:source:deploy -p force-app/ -u $1 -l RunSpecifiedTests -r TestF_PointsService -c -w 60
 fi
 
-#TO USE :
+# TO USE :
 # scripts/shell/deployProjectToOrg.sh username
